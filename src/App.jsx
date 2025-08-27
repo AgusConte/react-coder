edaeb6907e5c2b0a660ba09a5a4b2a42f440dbbd
agustin conte <agustinconte15@gmail.com>
2025-08-27 16:12:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from "./componentes/Navbar";
+import ItemListContainer from "./componentes/ItemListContainer";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Navbar />
+      <ItemListContainer greeting="Bienvenido a Factor BMX" />
+    </div>
+  );
+}
+
+export default App
---
+++ src/componentes/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ItemListContainer({ greeting }) {
+  return (
+    <section>
+      <h1>{greeting}</h1>
+    </section>
+  );
+}
+
+export default ItemListContainer;
